14ce73af251ee989d92b9a96d26cba960ac94bee
Victor Pessanha <victor.abreu.pessanha@gmail.com>
2020-07-28 21:17:13 -0300

    anibox

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/anibox_logo.png';
+//import ButtonLink from '../components/ButtonLink';
+
+import Button from '../Buttons'
+import './style.css'
+
+function Menu() {
+    return (
+        <nav className='Menu'>
+            <Link to='/'>
+                <img className='Logo' src={Logo} alt='AniBox logo'/>
+            </Link>
+
+            <Button as={Link} className='ButtonLink' to='/cadastro/video'> 
+                Novo vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Banner from '../../components/BannerMain/index';
+import Carousel from '../../components/Carousel/index';
+import PageDefault from '../../components/PageDefault';
+import dadosIniciais from '../../data/dados_iniciais.json'
+
+function Home() {
+  return (
+    <div>
+      <PageDefault>
+      <Banner 
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"Um dos melhores curtas de 2019. Ganhador do prêmio melhor animação, no canal Short of the Week"}
+      />
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}
+      />
+      </PageDefault>
+
+    </div>
+  );
+}
+
+export default Home;
