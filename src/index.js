88965cd7cee7e5a842d7ea7afe4a31bddf416e4c
Victor Pessanha <victor.abreu.pessanha@gmail.com>
2020-07-31 22:34:53 -0300

    Subir

--- src/index.js
+++ src/index.js
@@ -1,24 +1,76 @@
 import React from 'react';
+
+
+
 import ReactDOM from 'react-dom';
+
+
+
 import './reset.css';
+
+
+
 import './colors.css';
+
+
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
+
+
+
 import CadastroVideo from './pages/cadastro/Video';
+
+
+
 import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+
 import NotFound from './pages/NotFound';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 
 ReactDOM.render(
+
+
+
   <BrowserRouter>
+
+
+
     <Switch>
-      <Route path="/" component={Home} exact/>
-      <Route path="/cadastro/video" component={CadastroVideo} exact/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+
+
+
+      <Route path="/" component={Home} exact />
+
+
+
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+
+
+
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+
+
+
       <Route component={NotFound} />
 
+
+
     </Switch>
-    
+
+
+
   </BrowserRouter>,
-  document.getElementById('root')
+
+
+
+  document.getElementById('root'),
+
+
+
 );
+
